3a6ff8c7a5d3162d5ed1c105fe2607bc94e48fd4
noam ishay <noamishay1@gmail.com>
2021-03-20 17:19:56 +0200

    third commit

---
+++ text.py
@@ -0,0 +1,18 @@
+def revword(word:str):
+  return(word[::-1].lower())
+      
+def countword():
+    counter=1
+    text=open('text.txt').read()
+    text=text.replace('\n',' ')
+    text=(text.lower())
+    matword=text.split(' ')
+    x = len(matword)
+    i=0
+    while i<=x:
+       if revword(matword[0])==matword[i-1]:
+           counter=counter+1
+       i=i+1
+    print(float(counter))
+
+
